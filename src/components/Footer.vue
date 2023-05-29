0c03082af91f1dffeedef4649350024988da720a
Evgeniy Bochkarev <evgeniy.bochkarev@smartcomds.ru>
2023-05-29 17:32:49 +0700

    feat: BM-10628996 Add start template, tests

---
+++ src/components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+    <footer>
+        <p>Footer</p>    
+    </footer>
+</template>
+  
+<script setup lang="ts">
+
+</script>
+  
+<style scoped>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <header>
+    
+  </header>
+</template>
+  
+<script setup lang="ts">
+ 
+</script>
+  
+<style scoped>
+header {
+    height: 52px;
+    background-color: #353F55;
+    z-index: 3;
+    transition: 0.3s ease height;
+    overflow: hidden;
+    flex-shrink: 0;
+}
+</style>
