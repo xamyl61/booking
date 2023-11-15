c2e3cc84bd6b1ccb049353a45557ff871291f078
a.onishchenko <a.onishchenko@aleancollection.ru>
2023-11-15 20:31:19 +0300

    24679253 added space in timer and changed format to 00:00

--- src/features/booking/components/BookingPay.vue
+++ src/features/booking/components/BookingPay.vue
@@ -23,7 +23,7 @@
             <vue-countdown  :time="timeRemaining" v-slot="{ minutes, seconds }" @end="onCountdownEnd">
                 <div v-if="counting" class="flex justify-between ">
                     <div>ЕСЛИ В ТЕЧЕНИЕ 15 МИНУТ ВЫ НЕ ВНЕСЕТЕ ОПЛАТУ, БРОНЬ БУДЕТ АННУЛИРОВАНА</div>
-                    <div>{{ minutes }}:{{ seconds }}</div>
+                    <div>{{  String(minutes).padStart(2, '0') }}:{{ String(seconds).padStart(2, '0') }}</div>
                 </div>
                 <div v-else style="color: red">
                     Время вышло, бронь анулирована.
@@ -164,6 +164,10 @@ onMounted(async () => {
         line-height: 25px;
         text-transform: uppercase;
         margin: 1rem 0;
+
+        & > span {
+            width: 100%;
+        }
     }
     &__notification {
         color: #B91818;
