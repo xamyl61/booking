d824e54cb8a2a3efeffd2e0fbb7dd1acc4285f22
a.onishchenko <a.onishchenko@aleancollection.ru>
2023-12-03 21:49:46 +0300

    25089029 header menu on mobile

---
+++ src/components/icons/IconCloseMenu.vue
@@ -0,0 +1,8 @@
+<template>
+    <i class="icon icon-close-menu">
+        <svg width="17" height="18" viewBox="0 0 17 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect x="2.17819" y="0.514648" width="20.8892" height="3.07818" transform="rotate(45 2.17819 0.514648)" fill="#121326"/>
+            <rect x="17.0002" y="2.70898" width="20.9177" height="3.12426" transform="rotate(135 17.0002 2.70898)" fill="#121326"/>
+        </svg>
+    </i>
+</template>
